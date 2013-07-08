6172802957f7992e2fe786749249ab7c20621d4a
Ayumi Otomo <otomo.ayumi@dena.jp>
2013-07-08 17:48:10 +0900

    Initial Commit

---
+++ runner/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  runner
+//
+//  Created by Ayumi Otomo on 2013/07/08.
+//  Copyright Ayumi Otomo 2013年. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
